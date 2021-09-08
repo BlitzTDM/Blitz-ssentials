ba713a053ba08f462ae3dea1f32d401adc686d02
BlitzTDM <60582816+BlitzTDM@users.noreply.github.com>
2021-09-08 15:15:04 -0600

    Create chatcolors.java

---
+++ src/main/java/net/tydiumcraft/hiplugin/chatcolors.java
@@ -0,0 +1,11 @@
+package net.tydiumcraft.hiplugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class chatcolors {
+
+	public static String chat (String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+	
+}
