c9317415257f98d98dfbdc5940a7ab707d6b2878
BlitzTDM <60582816+BlitzTDM@users.noreply.github.com>
2021-09-08 15:11:58 -0600

    Create HiCommand.java

---
+++ src/main/java/net/tydiumcraft/hiplugin/HiCommand.java
@@ -0,0 +1,37 @@
+package net.tydiumcraft.hiplugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.tydiumcraft.hiplugin.Main;
+
+public class HiCommand implements CommandExecutor {
+
+	@SuppressWarnings("unused")
+	private Main plugin;
+	
+	public HiCommand(Main plugin) {
+		this.plugin = plugin;
+		plugin.getCommand("hi").setExecutor(this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] arg3) {
+		if (!(sender instanceof Player)){
+			sender.sendMessage(ChatColor.RED + "Only players may execute this command!");
+			return true;
+		}
+		Player p = (Player) sender;
+		
+		if (p.hasPermission("hi.use")) {
+			p.sendMessage(ChatColor.GREEN + "Hi!");
+			return true;
+		} else { 
+			p.sendMessage(ChatColor.RED + "No Permission!");	
+		}		
+			return false;
+	}
+}
